e03b12eaf2ce089e3a54fd4058240accdb1080ac
Cieslar, Bartlomiej <bartlomiej.cieslar20@imperial.ac.uk>
2021-10-18 14:33:59 +0000

    Reformatting system that doesn't necessarily reformat the whole codebase will in future be integrated with CI

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 CLEAN_SUBDIRS = src doc tests
+CHECKPATCH_IGNORES = SPDX_LICENSE_TAG,UNSPECIFIED_INT,STRLCPY,SIZEOF_PARENTHESIS,AVOID_EXTERNS,CONSIDER_COMPLETION,NEW_TYPEDEFS,PREFER_KERNEL_TYPES,MULTIPLE_ASSIGNMENTS,OPEN_ENDED_LINE
+GIT_BASE_COMMIT = 84de5c2bc75074d309e9efabf022b0afa80167a7
+GREP_GROUP_SEPARATOR = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 all::
 	@echo "This makefile has only 'clean' and 'check' targets."
@@ -11,3 +14,23 @@ distclean:: clean
 
 check::
 	make -C tests $@
+
+lint::
+	@files=$$(git diff $(GIT_BASE_COMMIT) --name-only 2>&1 | grep "\.[hc]$$");\
+	echo "###################" | tee lint.out >/dev/null;\
+	printf "$$files\n" | tee -a lint.out >/dev/null;\
+	echo "###################" | tee -a lint.out >/dev/null;\
+	./checkpatch.pl --strict --color=always -f --no-tree --ignore $(CHECKPATCH_IGNORES) --tab-size=2 --max-line-length=80 --show-types $$files 2>/dev/null\
+	| grep -e WARNING: -e ERROR: -e CHECK: -A 1 --group-separator=$(GREP_GROUP_SEPARATOR) \
+	| tr "\n" " " | sed "s/$(GREP_GROUP_SEPARATOR)/\n/g" \
+	| awk '{$$1=$$1;print}' | sed 's/.$$//' \
+	| grep -v -e PRI --no-group-separator \
+	| grep -v -e "No space is necessary after a cast" --no-group-separator \
+	| tee -a lint.out;\
+	rm -f .checkpatch-camelcase.git.*
+
+reformat::
+	@git diff $(GIT_BASE_COMMIT) --name-only 2>&1 | grep "\.[hc]$$" | while read file; \
+	do echo "$$file";\
+	python3 reformat.py $$file;\
+	done
---
+++ reformat.py
@@ -0,0 +1,51 @@
+from os import linesep
+import sys
+import subprocess
+
+subprocess.run(["clang-format","-style=file","-i", sys.argv[1]])
+
+#Reformats comments
+with open(sys.argv[1], "r") as f:
+    rawString = f.read()
+    inComment = False
+    output = ""
+
+    inComment = False
+    for line in rawString.splitlines():
+        commentStartPos = line.find("/*")
+        commentEndPos = line.find("*/")
+
+        if (commentStartPos != -1):
+            lastStartPos = commentStartPos
+
+        # /* One line Comment */
+        if (commentStartPos != -1 and commentEndPos != -1):
+            output += line.rstrip() + '\n'
+
+        # /* Beggining of block comment
+        elif (commentStartPos != -1 and commentEndPos == -1):
+            inComment = True
+            output += line.rstrip() + '\n'
+        
+        # End of block comment */
+        elif (commentStartPos == -1 and commentEndPos != -1):
+            inComment = False
+            if (line.lstrip()[0] != '*'):
+                output += ('\t' * lastStartPos) + " * " + line.replace('*/','').lstrip().rstrip() + '\n' + ('\t' * lastStartPos) + " */\n"
+            else:
+                output += line.rstrip() + '\n'
+        
+        # In the middle of a block comment
+        elif(inComment == True):
+            if (not line.lstrip().startswith('*')):
+                output += ('\t' * lastStartPos) + " * " +  line.lstrip() + '\n'
+            else:
+                output += line.rstrip() + '\n'
+        
+        #Normal line
+        else:
+            output += line.rstrip() + '\n'
+
+with open(sys.argv[1], "w") as f:
+    f.write(output)
+
